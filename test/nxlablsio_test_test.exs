b44e3e9c057b9f1f297bc3c10d9362b49106fc2b
jrhuasi <julianra@gmail.com>
2024-02-22 14:52:12 -0300

    Calentando motoresss

--- test/nxlablsio_test_test.exs
+++ test/nxlablsio_test_test.exs
@@ -2,21 +2,20 @@ defmodule NxlablsioTestTest do
 
   use ExUnit.Case
 
-  # doctest permite generar tests automáticos a partir de la documentación.
   doctest NxlablsioTest
 
-  # Test para verificar que la función retorna el nombre completo correctamente.
+  # Verificar si retorna el nombre completo correctamente.
   test "mostrar with :name returns full name" do
     assert NxlablsioTest.mostrar(:name) == "Sonoo Jaiswal"
   end
 
-  # Test para verificar que la función retorna la dirección completa correctamente.
+  # Verificar si retorna la dirección completa correctamente.
   test "mostrar with :address returns full address" do
     expected_address = "Plot-6, Mohan Nagar Ghaziabad UP 201007"
     assert NxlablsioTest.mostrar(:address) == expected_address
   end
 
-  # Test para verificar que la función calcula el año de nacimiento correctamente.
+  # Verificar si calcula el año de nacimiento correctamente.
   test "mostrar with :birth returns birth year" do
     current_year = Date.utc_today().year
     expected_birth_year = Integer.to_string(current_year - 27) # Usamos la edad directamente desde el JSON.
